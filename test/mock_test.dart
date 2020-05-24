57dab2ca1944bd54944fc473491621bbd62aecec
Danybeam <danybeam@gmail.com>
2020-05-24 12:12:44 -0700

    create coverage

--- test/mock_test.dart
+++ test/mock_test.dart
@@ -1,7 +1,9 @@
 import 'package:test/test.dart';
+import 'package:task_parser/task_parser.dart' as task;
 
 void main() {
   test("first test", () {
+    task.main([]);
     expect(true, true);
   });
 }
