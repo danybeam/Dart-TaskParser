8c648fc2c8a400658355ba126aa1c5c411a3e0b7
Daniel Orozco <danybeam@gmail.com>
2020-05-28 08:42:21 -0700

    add title test

--- test/title_test.dart
+++ test/title_test.dart
@@ -1,7 +1,11 @@
 import 'package:test/test.dart';
 
 void main() {
-  group('Title positive tests => ', () {});
+  group('Title positive tests => ', () {
+    test('parse title', () {
+      expect(true, true);
+    });
+  });
 
   group('Title negative tests => ', () {});
 }
