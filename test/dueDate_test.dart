486e63471fde62c2c3f5dd65ea1ec8d4f8be5db3
Daniel Orozco <danybeam@gmail.com>
2020-05-31 15:20:22 -0700

    add due date tests

--- test/dueDate_test.dart
+++ test/dueDate_test.dart
@@ -23,7 +23,56 @@ import 'package:task_parser/task_parser.dart' as parser;
 import 'package:tuple/tuple.dart';
 
 void main() {
-  group('Due Date positive tests => ', () {});
+  group('Due Date positive tests => ', () {
+    test('parse only due date no space', () {
+      Tuple2<DateTime, String> expected =
+          Tuple2(new DateTime(2020, 02, 01, 12, 34), "");
+      expect(parser.parseDueDate("@DueDate:2020-02-01T12:34"), expected);
+    });
 
-  group('Due Date negative tests => ', () {});
+    test('parse only due date with space', () {
+      Tuple2<DateTime, String> expected =
+          Tuple2(new DateTime(2020, 02, 01, 12, 34), "");
+      expect(parser.parseDueDate("@Due Date:2020-02-01T12:34"), expected);
+    });
+
+    test('parse due date with switch before', () {
+      Tuple2<DateTime, String> expected =
+          Tuple2(new DateTime(2020, 02, 01, 12, 34), "+foo");
+      expect(parser.parseDueDate("+foo @DueDate:2020-02-01T12:34"), expected);
+    });
+
+    test('parse due date with switch after', () {
+      Tuple2<DateTime, String> expected =
+          Tuple2(new DateTime(2020, 02, 01, 12, 34), "+foo");
+      expect(parser.parseDueDate("@DueDate:2020-02-01T12:34 +foo"), expected);
+    });
+  });
+
+  group('Due Date negative tests => ', () {
+    test('Raise error if the label is malformed', () {
+      Tuple2<DateTime, String> expected =
+          Tuple2(null, "@Duate:2020-02-01T12:34");
+      expect(parser.parseDueDate("@Duate:2020-02-01T12:34"), expected);
+    });
+
+    test('Raise error if the value is malformed (missing numbers)', () {
+      expect(() => parser.parseDueDate("@DueDate:2020-01T12:34"),
+          throwsFormatException);
+    });
+
+    test('Raise error if the value is malformed (missing value)', () {
+      expect(() => parser.parseDueDate("@DueDate"), throwsFormatException);
+    });
+
+    test('Raise error if the value is malformed (missing label)', () {
+      expect(() => parser.parseDueDate("@2020-02-01T12:34"),
+          throwsFormatException);
+    });
+
+    test('Raise error if value is malformed (letters in the middle)', () {
+      expect(() => parser.parseDueDate("@DueDate:asdf-02-01T12:34"),
+          throwsFormatException);
+    });
+  });
 }
