e7c520edd9b6287d94cd673ac84890d10b27fa21
Daniel Orozco <danybeam@gmail.com>
2020-06-08 20:54:22 -0700

    add interface and rename basic implementation

--- test/parser_test.dart
+++ test/parser_test.dart
@@ -4,22 +4,23 @@ import 'package:task_parser/task_parser.dart' as parser;
 void main() {
   group('Parse task positive tests => ', () {
     test('parse task with only title (dash)', () {
-      parser.Task expected = parser.Task(parser.states.dash, "foo");
+      parser.BasicTask expected = parser.BasicTask(parser.states.dash, "foo");
       expect(parser.parseTask("-foo"), expected);
     });
 
     test('parse task with only title (box)', () {
-      parser.Task expected = parser.Task(parser.states.box, "foo");
+      parser.BasicTask expected = parser.BasicTask(parser.states.box, "foo");
       expect(parser.parseTask("[ ]foo"), expected);
     });
 
     test('parse task with only title (checked box)', () {
-      parser.Task expected = parser.Task(parser.states.checked_box, "foo");
+      parser.BasicTask expected =
+          parser.BasicTask(parser.states.checked_box, "foo");
       expect(parser.parseTask("[x]foo"), expected);
     });
 
     test('parse task with everything', () {
-      parser.Task expected = parser.Task(parser.states.dash, "foo",
+      parser.BasicTask expected = parser.BasicTask(parser.states.dash, "foo",
           dueDate: DateTime(2020, 12, 31, 12, 34),
           description: "bar",
           properties: [parser.Property("baz", "buzz")],
@@ -38,7 +39,7 @@ void main() {
     });
 
     test('badly formated due date label act as property', () {
-      parser.Task expected = parser.Task(parser.states.dash, "foo",
+      parser.BasicTask expected = parser.BasicTask(parser.states.dash, "foo",
           properties: [parser.Property("dueate", "2020-12-31T12:34")]);
       expect(parser.parseTask("-foo@dueate:2020-12-31T12:34"), expected);
     });
